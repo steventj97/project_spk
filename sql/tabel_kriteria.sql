bc389c7ac8a47a233f700f9080290d3f0164c9fd
steventj97 <steventj97@gmail.com>
2018-03-22 18:09:59 +0700

    fifth commit

---
+++ sql/tabel_kriteria.sql
@@ -0,0 +1,5 @@
+INSERT INTO `project_spk`.`kriterias` (`id`, `kriteria`, `bobot`) VALUES ('1', 'biaya_parkiran', '0.2');
+INSERT INTO `project_spk`.`kriterias` (`id`, `kriteria`, `bobot`) VALUES ('2', 'kondisi_cuaca', '0.1');
+INSERT INTO `project_spk`.`kriterias` (`id`, `kriteria`, `bobot`) VALUES ('3', 'luas_tempat_parkir', '0.3');
+INSERT INTO `project_spk`.`kriterias` (`id`, `kriteria`, `bobot`) VALUES ('4', 'jarak_dari_kampus', '0.3');
+INSERT INTO `project_spk`.`kriterias` (`id`, `kriteria`, `bobot`) VALUES ('5', 'waktu_parkir', '0.1');
